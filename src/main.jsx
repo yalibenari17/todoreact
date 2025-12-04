f1334468b59a8a4bf0f97c4ff6c978c9f5ff1cea
yalibenari17 <yalibenari@gmail.com>
2025-12-04 18:02:58 +0200

    Fix Firestore rules + CSS import

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
-import "./style.css";
+import "./style.css";   // ← חשוב!
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
